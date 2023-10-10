a8e259d99f250ca29a0dae789bb786f82dd524e8
bitcode001 <82773560+bitcode001@users.noreply.github.com>
2023-10-10 23:40:33 +0100

    feat(): play around with drag drop api of the DOM element

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,190 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import {
-	// handleDragOver,
+	// handleCardDragOver,
+	handleDragOver,
 	handleDragStart,
-	handleDrop,
+	// handleOnDrag,
+	// handleOnDragStart,
+	// handleDrop,
 	handleTempCardDrag,
 	handleTempCardDragEnd
 } from './utils/custom-drag-handler';
 
-const blueItems = Array.from({ length: 5 }, (_, idx) => idx);
-const greenItems = Array.from({ length: 5 }, (_, idx) => idx + 5);
+// const blueItems = Array.from({ length: 5 }, (_, idx) => idx);
+// const greenItems = Array.from({ length: 5 }, (_, idx) => idx + 5);
+export type KanbanBoardKeys = 'backlog' | 'in-progress';
+type KanbanState = {
+	[k in KanbanBoardKeys]: number[];
+};
+
+const enum KanbanDispathType {
+	UPDATE_BACKLOG,
+	UPDATE_IN_PROGRESS
+}
+interface KanbanAction {
+	type: KanbanDispathType;
+	payload: number[];
+}
+// Create a reducer function
+const kanbanReducer = (state: KanbanState, action: KanbanAction): KanbanState => {
+	switch (action.type) {
+		case KanbanDispathType.UPDATE_BACKLOG:
+			return {
+				...state,
+				backlog: action.payload
+			};
+		case KanbanDispathType.UPDATE_IN_PROGRESS:
+			return {
+				...state,
+				'in-progress': action.payload
+			};
+		default:
+			return state;
+	}
+};
 
 function App() {
+	// Initial state
+	const initialState: KanbanState = {
+		backlog: Array.from({ length: 5 }, (_, idx) => idx),
+		'in-progress': Array.from({ length: 5 }, (_, idx) => idx + 5)
+	};
+
+	// useReducer hook
+	const [kanbanData, dispatch] = useReducer(kanbanReducer, initialState);
+
+	// const [draggedIndex, setDraggedIndex] = useState<number | null>(null);
+	// const [dragOverIndex, setDragOverIndex] = useState<number | null>(null);
+
+	// const handleDragEndCleanUp = () => {
+	// 	setDraggedIndex(null);
+	// 	setDragOverIndex(null);
+	// };
+
+	const handleCardDragLogic = (cont: KanbanBoardKeys, a: string, b: string) => {
+		// const aa = kanbanData[cont].indexOf(Number(a));
+		const bb = kanbanData[cont].indexOf(Number(b));
+
+		console.log('Container: ', cont);
+		console.log('a: ', a);
+		console.log('b: ', b);
+
+		const tempCopy = kanbanData[cont];
+		const final = tempCopy.filter((el) => el !== Number(a));
+		console.log('part a:', final);
+		console.log('index bb:', bb);
+		final.splice(bb, 0, Number(a));
+
+		console.log('Formatted ans: ', final);
+		console.log('Dispatching: ', {
+			type:
+				cont === 'backlog'
+					? KanbanDispathType.UPDATE_BACKLOG
+					: KanbanDispathType.UPDATE_IN_PROGRESS,
+			payload: final
+		});
+
+		dispatch({
+			type:
+				cont === 'backlog'
+					? KanbanDispathType.UPDATE_BACKLOG
+					: KanbanDispathType.UPDATE_IN_PROGRESS,
+			payload: final
+		});
+
+		// Cleanaup
+		// handleDragEndCleanUp();
+	};
+
 	return (
 		<React.Fragment>
-			<h1>Welcome to Superpower Kanban</h1>
+			<h1 className="py-4 text-center">Welcome to Superpower Kanban</h1>
 			<div
 				draggable="true"
-				onDragStart={handleDragStart}
-				className="draggable h-10 w-10 bg-yellow-500"
-			></div>
+				// onDragStart={handleDragStart}
+				// onDrop={handleDrop}
+				className="draggable m-auto flex w-fit bg-yellow-500 p-2"
+			>
+				Drag Me
+			</div>
 			<div className="flex flex-row">
 				<div
 					className="m-10 h-full flex-1 bg-blue-500"
 					id="container-a"
-					// onDragOver={handleDragOver}
+					data-sk-cont={'backlog'}
+					data-sk-cont-idx={0}
+					// draggable={true}
+					onDragStart={handleDragStart}
+					onDragOver={handleDragOver}
 					// onDragEnd={handleDrop}
-					onDrop={handleDrop}
+					// onDrop={handleDrop}
 					onDrag={handleTempCardDrag}
-					onDragEnd={handleTempCardDragEnd}
+					onDragEnd={(e: React.DragEvent<HTMLDivElement>) => {
+						handleTempCardDragEnd(e, (cont: KanbanBoardKeys, a: string, b: string) =>
+							handleCardDragLogic(cont, a, b)
+						);
+					}}
 				>
-					{blueItems.map((el) => (
+					{kanbanData.backlog.map((el) => (
 						<div
 							draggable={true}
-							className="m-2 flex h-10 flex-1 items-center justify-center bg-white py-4 opacity-100 transition-all"
+							className="relative flex cursor-pointer items-center justify-center bg-transparent px-2 py-4 opacity-100"
 							key={el}
+							data-id={el}
 							// onDrag={handleTempCardDrag}
 							// onDragEnd={handleTempCardDragEnd}
 						>
-							{el}
+							<div className="flex-1 bg-white py-2 text-center">{el}</div>
 						</div>
 					))}
 				</div>
 				<div
 					className="m-10 h-full flex-1 bg-green-500"
 					id="container-b"
+					data-sk-cont={'in-progress'}
+					data-sk-cont-idx={1}
 					// draggable={true}
-					// onDragOver={handleDragOver}
+					onDragStart={handleDragStart}
+					onDragOver={handleDragOver}
 					// onDragEnd={handleDrop}
-					onDrop={handleDrop}
+					// onDrop={handleDrop}
 					onDrag={handleTempCardDrag}
-					onDragEnd={handleTempCardDragEnd}
+					onDragEnd={(e: React.DragEvent<HTMLDivElement>) => {
+						handleTempCardDragEnd(e, (cont: KanbanBoardKeys, a: string, b: string) =>
+							handleCardDragLogic(cont, a, b)
+						);
+					}}
 				>
-					{greenItems.map((el) => (
+					{kanbanData['in-progress'].map((el) => (
 						<div
 							draggable={true}
-							className="m-2 flex h-10 flex-1 items-center justify-center bg-white py-4 opacity-100 transition-all"
+							className="relative flex cursor-pointer items-center justify-center bg-transparent px-2 py-4 opacity-100"
 							key={el}
+							data-id={el}
 							// onDrag={handleTempCardDrag}
 							// onDragEnd={handleTempCardDragEnd}
 						>
-							{el}
+							<div className="flex-1 bg-white py-2 text-center">{el}</div>
 						</div>
 					))}
 				</div>
 			</div>
+
+			{/* <div className="drag-container mb-5" onDragStart={handleOnDragStart} onDrag={handleOnDrag}>
+				{[1, 2, 3, 4, 5].map((el) => (
+					<div draggable={true} className="draggable" key={el}>
+						{el}
+					</div>
+				))}
+			</div> */}
+			<button
+				className="btn m-auto flex bg-indigo-500 p-4"
+				onClick={() => {
+					console.log('KanbanData: ', kanbanData);
+				}}
+			>
+				Show Data
+			</button>
 		</React.Fragment>
 	);
 }
--- src/utils/custom-drag-handler.ts
+++ src/utils/custom-drag-handler.ts
@@ -1,10 +1,23 @@
+import { KanbanBoardKeys } from '@/App';
 import { DragEventHandler } from 'react';
+
+let item_getting_dragged: HTMLElement | null;
+let hoverOnRef: Element | null;
 
 export const handleDragStart: DragEventHandler<HTMLDivElement> = (
 	e: React.DragEvent<HTMLDivElement>
 ) => {
 	e.stopPropagation();
-	console.log('Drag start: ', e.target);
+	// console.log('Drag start: ', e.target);
+
+	// item_getting_dragged = document.elementFromPoint(e.pageX, e.pageY);
+	item_getting_dragged = e.target as HTMLElement;
+
+	e.currentTarget.classList.add('grabbing-icon');
+
+	for (let i = 0; i < e.currentTarget.children.length; i++) {
+		e.currentTarget.children[i].classList.add('transition-all');
+	}
 };
 
 export const handleDragOver: DragEventHandler<HTMLDivElement> = (
@@ -12,7 +25,6 @@ export const handleDragOver: DragEventHandler<HTMLDivElement> = (
 ) => {
 	e.stopPropagation();
 	e.preventDefault();
-	console.log('Drag over: ', e);
 };
 
 export const handleDrop: DragEventHandler<HTMLDivElement> = (
@@ -20,31 +32,155 @@ export const handleDrop: DragEventHandler<HTMLDivElement> = (
 ) => {
 	e.stopPropagation();
 	e.preventDefault();
-	console.log('Drop : ', e);
+	console.log('Dropped : ', e.target);
 };
 
-export const handleTempCardDrag: DragEventHandler<HTMLDivElement> = (
-	e: React.DragEvent<HTMLDivElement>
-) => {
-	//   e.stopPropagation();
+export const handleTempCardDrag: (e: React.DragEvent<HTMLDivElement>) => void = (e) => {
+	e.stopPropagation();
+	e.preventDefault();
 	const delegatedEl = e.target as HTMLElement;
 	if (delegatedEl.classList.contains('opacity-100')) {
 		delegatedEl.classList.remove('opacity-100');
-		delegatedEl.classList.add('opacity-50');
+		delegatedEl.classList.add('opacity-0');
 	}
-	//   console.log("e current target: ", e.currentTarget);
-	//   console.log("e target: ", (e.target as HTMLElement).classList);
-	//   console.log("Card e: ");
+
+	// Get items from the current mouse position
+	const hoverOnItem = document.elementFromPoint(e.pageX, e.pageY);
+
+	// Safety check gate
+	if (
+		hoverOnItem !== null &&
+		hoverOnItem !== e.target &&
+		hoverOnItem.parentNode === e.currentTarget
+	) {
+		// Get bounding client rect of that box
+		const hoveredOnItem = hoverOnItem.getBoundingClientRect();
+
+		// Calculate the overlapping space between the div I am draggin against the div I am hovering on
+		if (e.pageY > hoveredOnItem.top + hoveredOnItem.height / 2) {
+			// console.log('hover on item: ', hoverOnItem);
+			if (hoverOnRef !== hoverOnItem) {
+				const childLists: HTMLElement[] = Array.prototype.slice.call(
+					e.currentTarget.children
+				) as HTMLElement[];
+				const myHoveredIndex = childLists.indexOf(hoverOnItem as HTMLElement);
+				const grabbedIndex = childLists.indexOf(item_getting_dragged as HTMLElement);
+
+				let deltaY: 'down' | 'up' | 'still';
+				if (grabbedIndex < myHoveredIndex) {
+					deltaY = 'down';
+				} else if (grabbedIndex > myHoveredIndex) {
+					deltaY = 'up';
+				} else if (grabbedIndex === myHoveredIndex) {
+					deltaY = 'still';
+				} else {
+					deltaY = 'down';
+				}
+
+				// console.log('Swap space');
+				// console.log('dletaY:', deltaY);
+				// console.log('grabbed index:', grabbedIndex);
+				// console.log('hovered index:', myHoveredIndex);
+
+				// console.log('diff: ', item_getting_dragged?.y, hoveredOnItem.y);
+				// console.log('delta Y: ', e.clientY);
+
+				// cleanup stale property element
+				childLists.map((el) => el.removeAttribute('style'));
+
+				if (deltaY === 'up')
+					childLists.slice(myHoveredIndex, grabbedIndex).map((el) =>
+						// !el.getAttribute('style') &&
+						el.setAttribute(
+							'style',
+							`transform: translateY(${el.getBoundingClientRect().height}px)`
+						)
+					);
+
+				if (deltaY === 'down')
+					childLists.slice(grabbedIndex, myHoveredIndex + 1).map((el, idx) => {
+						// !el.getAttribute('style') &&
+						el.getAttribute('style') && el.removeAttribute('style');
+						idx > 0 &&
+							el.setAttribute(
+								'style',
+								`transform: translateY(-${el.getBoundingClientRect().height}px)`
+							);
+					});
+
+				// if (deltaY === 'still') childLists.map((el) => el.removeAttribute('style'));
+			} else {
+				// If user keep on hovering on same item
+			}
+
+			hoverOnRef = hoverOnItem;
+		}
+	}
+
+	if (hoverOnItem === e.target) {
+		// console.log('I am hovering myself');
+		// (Array.prototype.slice.call(e.currentTarget.children) as HTMLElement[]).map((el) =>
+		// 	el.removeAttribute('style')
+		// );
+	}
 };
 
-export const handleTempCardDragEnd: DragEventHandler<HTMLDivElement> = (
-	e: React.DragEvent<HTMLDivElement>
-) => {
-	//   e.stopPropagation();
+export const handleTempCardDragEnd: (
+	e: React.DragEvent<HTMLDivElement>,
+	cb: (cont: KanbanBoardKeys, a: string, b: string) => void
+) => void = (e, cb) => {
+	e.stopPropagation();
 	const delegatedEl = e.target as HTMLElement;
-	if (delegatedEl.classList.contains('opacity-50')) {
-		delegatedEl.classList.remove('opacity-50');
+	if (delegatedEl.classList.contains('opacity-0')) {
+		delegatedEl.classList.remove('opacity-0');
 		delegatedEl.classList.add('opacity-100');
 	}
-	//   console.log("Card e: ");
+
+	// Get the container
+	const container = e.currentTarget;
+	const container_key = (container.getAttribute('data-sk-cont') as KanbanBoardKeys) ?? 'backlog';
+
+	// Remove transition
+	for (let i = 0; i < e.currentTarget.children.length; i++) {
+		e.currentTarget.children[i].classList.remove('transition-all');
+		container.children[i].removeAttribute('style');
+	}
+
+	// Removing grabbed icon
+	if (item_getting_dragged) item_getting_dragged.classList.add('cursor-pointer');
+
+	document.body.classList.remove('grabbing-icon');
+
+	if (hoverOnRef) {
+		// console.log('THROW: ', hoverOnRef);
+		cb(
+			container_key,
+			(e.target as HTMLElement).getAttribute('data-id') ?? '0',
+			hoverOnRef.getAttribute('data-id') ?? '0'
+		);
+	}
+
+	hoverOnRef = null;
+	item_getting_dragged = null;
+};
+
+export const handleCardDragOver: DragEventHandler<HTMLDivElement> = (
+	e: React.DragEvent<HTMLDivElement>
+) => {
+	// To prevent event bubbling
+	e.stopPropagation();
+	// To Allow to be dropped
+	e.preventDefault();
+	console.log('Drag over: ', e.target);
+};
+
+// NEW TEST
+export const handleOnDrag = (e: React.DragEvent) => {
+	e.stopPropagation();
+	// console.log('e.target: ', e.target);
+};
+
+export const handleOnDragStart = (e: React.DragEvent) => {
+	e.stopPropagation();
+	console.log('e.target: ', e.target);
 };
